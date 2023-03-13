4acd88265e743837d2756d41e2fabaa2416b0e06
Agrim <agrimsharma1714@gmail.com>
2023-03-13 23:50:52 +0530

    completed the contract

--- To-Do-List-dApp/backend/contracts/todo.sol
+++ To-Do-List-dApp/backend/contracts/todo.sol
@@ -3,10 +3,25 @@ pragma solidity ^0.8.9;
 
 contract todo {
     
-    string todo[];
+    string public todo[];
 
     function setTodo(string memory _todo) public {
         todo.push(_todo);
+    }
+
+    function getTodo() public view returns(string[] memory) {
+        return todo;
+    }
+
+    function getTodoLength() public view returns(uint256 ) {
+        uint256 lengthOFTodo = todo.length();
+        return lengthOFTodo;
+    }
+
+    function deleteTodo(uint256 _index) public {
+        require(_index < todo.length(), "The index doesn't exist");
+        todo[_index] = todo[todo.length() - 1];
+        todo.pop();
     }
 
 }
