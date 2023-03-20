d666f45a85f9324e6daf28b219fd6aedfc4e6d31
Agrim <agrimsharma1714@gmail.com>
2023-03-20 23:59:20 +0530

    add deploy file

--- Expense Tracker dApp/backend/scripts/deploy.js
+++ Expense Tracker dApp/backend/scripts/deploy.js
@@ -1,32 +1,26 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const {ethers} = require("hardhat");
+require("dotenv").config({ path: ".env" });
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
 
-  const lockedAmount = hre.ethers.utils.parseEther("0.001");
+  // A contract factory in ethers is an abstraction used to deploy new smart contracts,
+  // so ExpenseTrackerContract here is a factory of instances of our Expense_Tracker contract.
+  const ExpenseTrackerContract = await ethers.getContractFactory('Expense_Tracker');
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  //deployment of contract
+  const deployedExpenseTrackerContract = await ExpenseTrackerContract.deploy();
 
-  await lock.deployed();
+  await deployedExpenseTrackerContract.deployed(); //waiting for contract to deploy
 
-  console.log(
-    `Lock with ${ethers.utils.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  //printing the address of the deployed contract
+  console.log("Contract deployed to address:", deployedExpenseTrackerContract.address);
+
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+// invokation of the main function and checking for errors in the code below
+main()
+.then(() => process.exit(0))
+.catch((error) => {
+    console.error(error);
+    process.exit(1);
+})
