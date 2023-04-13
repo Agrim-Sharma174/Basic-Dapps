5b98abeec9f913dbbfde84cccfe6a609f2d18c0f
Agrim <agrimsharma1714@gmail.com>
2023-04-13 23:39:02 +0530

    contract in progress

--- Vending-Machine-dApp/backend/contracts/Vending_Machine.sol
+++ Vending-Machine-dApp/backend/contracts/Vending_Machine.sol
@@ -2,5 +2,14 @@
 pragma solidity ^0.8.9;
 
 contract Vending_Machine {
-    
+    //two state variables
+    address public owner;
+    mapping(address => uint) public donutBalance;
+
+    // setting state in constructor
+    constructor() {
+        owner = msg.sender;
+        donutBalance[address(this)] = 100;
+    }
+
 }
