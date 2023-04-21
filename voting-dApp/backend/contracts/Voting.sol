6f557d53e47653da9a1c0be699940341023fef87
Agrim <agrimsharma1714@gmail.com>
2023-04-21 23:59:55 +0530

    vending machine dapp started

--- voting-dApp/backend/contracts/Voting.sol
+++ voting-dApp/backend/contracts/Voting.sol
@@ -2,16 +2,7 @@
 pragma solidity ^0.8.9;
 
 contract voting {
-    struct Candidate {
-        string name;
-        uint256 votes_count;
-    }
-
-    uint public candidatesCount;
-    address public voter;
-    address[] public addressesArray;
-
-    // mapping id => Candidate name and votes count
+   
 
     
 
