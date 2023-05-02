16a25196db13f3db10d5e43af8b4d9c5cef37ebc
Agrim <agrimsharma1714@gmail.com>
2023-05-02 18:00:36 +0530

    frontend work in progress

---
+++ FIR dApp/frontend/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <div className='flex justify-between m-5'>
+            <div>Logo</div>
+            <div>FIR PORTAL</div>
+            <div>
+                <button>
+                    Connect Wallet!
+                </button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
