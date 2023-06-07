24eb3154fbd4a5ddb263fb9ec364c388cbe4c604
Agrim <agrimsharma1714@gmail.com>
2023-06-07 22:34:59 +0530

    changes in blog dApp

---
+++ Blog-dApp/frontend/constants/constants.js
@@ -0,0 +1,141 @@
+export const BlogContractAddress = '0x630D25623dB1d47d40b33D95a8e98752CE64358F'
+export const BlogContractAbi = [
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "name": "blogArray",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "title",
+        "type": "string"
+      },
+      {
+        "internalType": "string",
+        "name": "typeOf",
+        "type": "string"
+      },
+      {
+        "internalType": "string",
+        "name": "url",
+        "type": "string"
+      },
+      {
+        "internalType": "string",
+        "name": "description",
+        "type": "string"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "_index",
+        "type": "uint256"
+      }
+    ],
+    "name": "deleteBlog",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "getBlog",
+    "outputs": [
+      {
+        "components": [
+          {
+            "internalType": "string",
+            "name": "title",
+            "type": "string"
+          },
+          {
+            "internalType": "string",
+            "name": "typeOf",
+            "type": "string"
+          },
+          {
+            "internalType": "string",
+            "name": "url",
+            "type": "string"
+          },
+          {
+            "internalType": "string",
+            "name": "description",
+            "type": "string"
+          }
+        ],
+        "internalType": "struct Blog.blog[]",
+        "name": "",
+        "type": "tuple[]"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "string",
+        "name": "_title",
+        "type": "string"
+      },
+      {
+        "internalType": "string",
+        "name": "_typeOf",
+        "type": "string"
+      },
+      {
+        "internalType": "string",
+        "name": "_url",
+        "type": "string"
+      },
+      {
+        "internalType": "string",
+        "name": "_description",
+        "type": "string"
+      }
+    ],
+    "name": "setBlog",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "string",
+        "name": "_title",
+        "type": "string"
+      },
+      {
+        "internalType": "string",
+        "name": "_typeOf",
+        "type": "string"
+      },
+      {
+        "internalType": "string",
+        "name": "_url",
+        "type": "string"
+      },
+      {
+        "internalType": "string",
+        "name": "_description",
+        "type": "string"
+      }
+    ],
+    "name": "updateBlog",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  }
+]
--- Blog-dApp/frontend/pages/index.js
+++ Blog-dApp/frontend/pages/index.js
@@ -1,124 +1,184 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import React, {useState, useRef, useEffect} from 'react'
+import Navbar from './Components/Navbar'
+import AddBlog from './Components/AddBlog'
+import BlogCard from './Components/BlogCard'
+import Web3Modal from 'web3modal'
+import { providers, Contract } from 'ethers'
+import { BlogContractAddress, BlogContractAbi } from '../constants/constants'
 
-const inter = Inter({ subsets: ['latin'] })
+const Home = () => {
+  // const [imgUrl, setImgUrl] = useState('')
+  const [blogs, setBlogs] = useState([])
+  const [darkMode, setDarkMode] = useState(false)
+  const [modal, setModal] = useState(false)
+  // Will keep track of wallet connection 
+  const [walletConnected, setWalletConnected] = useState(false)
+  // will be used when we are waiting for a transaction to be mined
+  const [loading, setLoading] = useState(false)
+  // create a reference to the web3Modal 
+  const web3ModalRef = useRef();
 
-export default function Home() {
+  const getProviderOrSigner = async (needSigner = false) => {
+    // Connect to Metamask
+    // Since we store `web3Modal` as a reference, we need to access the `current` value to get access to the underlying object
+    const provider = await web3ModalRef.current.connect();
+    const web3Provider = new providers.Web3Provider(provider)
+
+    const { chainId } = await web3Provider.getNetwork()
+    // if the user is not connected to the correct network throw an error and let them know which network they should connect to
+    if(chainId !== 11155111) {
+      window.alert("Change the network to sepolia");
+      throw new Error("Change Network to sepolia Testnet")
+    }
+    
+    if(needSigner) {
+      const signer = web3Provider.getSigner();
+      return signer;
+    }
+    return web3Provider;
+  }
+
+  const addBlogPosts = async (url, title, type, description) => {
+    // we need a signer here since this is a write transaction 
+    const signer = await getProviderOrSigner(true);
+    const contract = new Contract(
+      BlogContractAddress, 
+      BlogContractAbi,
+      signer
+    )
+    // fetching addBlogPost() function from the contract and passing in the necessary parameters
+    const addBlogs = await contract.addBlogPosts(url, title, type, description);
+    setLoading(true);
+    // waiting for the transaction to be mined
+    await addBlogs.wait();
+    getBlogPosts();
+    setLoading(false);
+    console.log("addBlogs", addBlogs)  
+  }
+
+  const getBlogPosts = async () => {
+    try {
+      // Using a provider since we are reading from smart contract
+      const provider = await getProviderOrSigner();
+      const contract = new Contract(
+        BlogContractAddress,
+        BlogContractAbi,
+        provider
+        )
+        // fetching getBlogPost() function from the contract
+        const getBlogs = await contract.getBlogPosts();
+        setBlogs(getBlogs)
+        console.table(blogs)
+      }
+      catch(err){
+        console.error(err)
+      }
+      
+    }
+
+    const deleteBlogPosts = async (index) => {
+      try{
+        const signer = await getProviderOrSigner(true);
+        const contract = new Contract(
+          BlogContractAddress,
+          BlogContractAbi,
+          signer
+        )
+        // passing in the parameter to delete the particular selected blog
+        const deleteBlogs = await contract.deleteBlogPosts(index);
+        setLoading(true);
+        await deleteBlogs.wait();
+        setLoading(false)
+        getBlogPosts();
+      }
+      catch(err){
+        console.error(err)
+      }
+
+    }
+
+
+    console.log(blogs)
+
+    // mapping through the blogs array which consists of the data and render the BlogCard component 
+    const getDynamicBlogs = blogs.map((blog, index) => {
+      return <BlogCard key={index} darkMode={darkMode} {...blog} index={index} deleteBlogPosts={deleteBlogPosts}
+      imgUrl={blog.url}
+      />
+    })
+
+
+  const connectWallet = async () => {
+      try{
+        // get the provider from web3Modal which in our case is MetaMask
+        // When used for the first time it prompts the user to connect their wallet
+        await getProviderOrSigner();
+        setWalletConnected(true);
+      }
+
+      catch(err) {
+        console.error(err)
+      }
+  }
+
+  useEffect(() => {
+    // if wallet is not connected create a new instance of web3Modal and connect the wallet
+    if(!walletConnected) {
+      // Assign the Web3Modal class to the reference object by setting it's `current` value
+      // The `current` value is persisted throughout as long as this page is open
+      web3ModalRef.current = new Web3Modal({
+        network: "sepolia",
+        providerOptions: {},
+        disableInjectedProvider: false
+      })
+      connectWallet()
+      getBlogPosts();
+    }
+
+  }, [walletConnected])
+
+
+  const toggleModal = () => {
+    setModal(prevState => !prevState)
+  }
+
+  
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <main
+      className={`${darkMode && 'dark'} dark:bg-[#10172a]`}
+      >
+        <Navbar 
+        darkMode={darkMode}
+        setDarkMode={setDarkMode}
+        toggleModal={toggleModal}
         />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+        <div
+        className="dark:bg-[#10172a]"
         >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          {modal && 
+          <AddBlog 
+          toggleModal={toggleModal}
+          addBlogPosts={addBlogPosts}
+          />
+        }
+       
+          </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+        <div
+        className="flex flex-wrap dark:bg-[#10172a]"
         >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
+        {getDynamicBlogs}
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+        </div>
+        <button
+        onClick={getBlogPosts}
         >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+
+        </button>
+      </main>
+    
   )
 }
+
+export default Home
