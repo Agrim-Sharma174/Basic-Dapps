77ea276f759823f00f854b99c318aa8e9dd656ec
Agrim <agrimsharma1714@gmail.com>
2023-06-16 23:44:14 +0530

    voting dApp

--- voting dApp/backend/contracts/Vote.sol
+++ voting dApp/backend/contracts/Vote.sol
@@ -3,5 +3,18 @@
 pragma solidity ^0.8.9;
 
 contract Vote {
-    
+    struct Candidate{
+        string name;
+        uint voteCount;
+    }
+    constructor() {
+        addCandidate("Agrim");
+        addCandidate("Sharma");
+    }
+    addCandidate(string memory _name) public {
+        uint256 _id = candidatesCount ;
+        candidates[_id] = Candidate(_candidateName, 0);
+        candidatesCount++ ;
+        return _id ;
+    }
 }
