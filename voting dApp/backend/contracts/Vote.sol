1b79ca02351cc4c0fdc6c880ebf845a07a922238
Agrim <agrimsharma1714@gmail.com>
2023-10-01 23:53:16 +0530

    voting dapp

--- voting dApp/backend/contracts/Vote.sol
+++ voting dApp/backend/contracts/Vote.sol
@@ -3,21 +3,36 @@
 pragma solidity ^0.8.9;
 
 contract Vote {
-    struct Candidate{
+    struct Candidate {
         string name;
-        uint voteCount;
+        uint256 votes_count;
     }
+
+    uint public candidatesCount;
+    address public voter;
+    address[] public addressesArray;
+
+    ///  mapping id => Candidate name and Vote count 
+
+
+    mapping(uint => Candidate) public candidates;
+    mapping(address => bool) public voters;
+
     constructor() {
-        addCandidate("Agrim");
-        addCandidate("Sharma");
+        addCandidate("Orange Julius");
+        // 0 => Orange Julius
+        addCandidate("Stuttering Joe");
+        // 1 => Stuttering Joe
     }
-    addCandidate(string memory _name) public {
+    
+    function addCandidate(string memory _candidateName) private returns(uint id){
         uint256 _id = candidatesCount ;
         candidates[_id] = Candidate(_candidateName, 0);
         candidatesCount++ ;
         return _id ;
     }
-        function vote(uint _id) public {
+
+    function vote(uint _id) public {
         require(!voters[msg.sender], "You have already voted once!");
         require(_id >= 0 && _id <= candidatesCount);
         voters[msg.sender] = true;
@@ -29,7 +44,16 @@ contract Vote {
     function getAddressesArray() public view returns(address[] memory) {
         return addressesArray;
     }
+
     function getAddressesArrayLength() public view returns(uint) {
         return addressesArray.length;
+    }
+
+    function countVote(uint256 id) public view returns(uint256 votes) {
+        return candidates[id].votes_count;
+    }
+
+    function getCandidate(uint256 id) public view returns(string memory _name) {
+        return candidates[id].name;
     }
 }
